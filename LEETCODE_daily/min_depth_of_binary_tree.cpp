f7bc274c24cb806b53fefa8c44113b1bd4e77a74
Sakshi Thombre <84434398+Sakshy18@users.noreply.github.com>
2023-07-14 15:28:02 +0530

    done nothing

--- LEETCODE_daily/min_depth_of_binary_tree.cpp
+++ LEETCODE_daily/min_depth_of_binary_tree.cpp
@@ -3,3 +3,11 @@
 // Note: A leaf is a node with no children.
 // Input: root = [3,9,20,null,null,15,7]
 // Output: 2
+
+ int minDepth(TreeNode* root) {
+    if(root==NULL) return 0;
+    else if(root->right==NULL && root->left==NULL) return 1;
+    else if(root->left==NULL) return minDepth(root->right)+1;
+    else if(root->right==NULL) return minDepth(root->left)+1;
+    return min(minDepth(root->left),minDepth(root->right)) +1; 
+    }
